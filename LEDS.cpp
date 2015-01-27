467859aa05e99a0e6ef70f54d8cbeff363ebb852
ecekat <sotiris-n@hotmail.com>
2015-01-28 01:04:30 +0200

    Update LEDS.cpp

--- LEDS.cpp
+++ LEDS.cpp
@@ -4,6 +4,17 @@
 
 class LED{
   private:
-  string path;
-  int number;
-  virtual void 
+    string path;
+    int number;
+    virtual void writeLED(string filename, string value);
+    virtual void removeTrigger();
+  public:
+    LED(int number);
+    virtual void turnOn();
+    virtual void turnOff();
+    virtual void flash(string delaynums);
+    virtyal void outputState();
+    virtual ~LED();
+};
+
+  
